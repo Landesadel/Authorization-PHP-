e48a01bcac26fe70af5ade1af78cd4f3f37f63b5
Landesadel <iliillimusicforever@gmail.com>
2022-08-31 16:13:28 +0400

    .

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Authorization</title>
+    <link rel="stylesheet" href="./src/style/style.css">
+</head>
+<body>
+    <form class="Login-form" action="" method="">
+        <label>Mail</label>
+        <input type="text">
+        <label>Password</label>
+        <input type="password">
+        <button>Login</button>
+    </form>
+</body>
+</html>
+
+
+
